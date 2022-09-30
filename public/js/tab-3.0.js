f3b7f00d828d27d0aeb838af033d11683439e7e4
user.name <gcruzcor@iu.edu>
2022-09-30 15:44:50 -0400

    #nearby css

---
+++ public/js/tab-3.0.js
@@ -0,0 +1,14 @@
+function togSchools() {
+    var element = document.getElementById("togSchool");
+    element.classList.toggle("show1");
+ }
+
+ function togPlay() {
+    var element = document.getElementById("togPlay");
+    element.classList.toggle("show2");
+ }
+
+ function togParks() {
+    var element = document.getElementById("togParks");
+    element.classList.toggle("show3");
+ }
