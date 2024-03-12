311fd307a1f12bc2efb1503f8cadb376b0e324b0
jhvhs-bloom <121638765+jhvhs-bloom@users.noreply.github.com>
2024-03-12 17:06:54 -0400

    map added without a marker
    marker will need to be added later

--- public/js/map-3.0.js
+++ public/js/map-3.0.js
@@ -1,16 +1,32 @@
 'use strict';
-const coords = document.getElementById('coordinates');
-const point  = { lat: parseFloat(coords.dataset.latitude), lng: parseFloat(coords.dataset.longitude) };
-let   map    = new google.maps.Map(document.getElementById("map"), {
-                   center:    point,
-                   zoom:      17,
-                   mapTypeId: 'satellite'
-               });
-const marker = new google.maps.Marker({
-                   position: point,
-                   map: map,
-                   icon: {
-                       url: BASE_URI + '/js/images/map-crosshair.png',
-                       anchor: { x: 35, y: 35 }
-                   }
-               });
+// const coords = document.getElementById('coordinates');
+// const point  = { lat: parseFloat(coords.dataset.latitude), lng: parseFloat(coords.dataset.longitude) };
+// let   map    = new google.maps.Map(document.getElementById("map"), {
+//                    center:    point,
+//                    zoom:      17,
+//                    mapTypeId: 'satellite'
+//                });
+// const marker = new google.maps.Marker({
+//                    position: point,
+//                    map: map,
+//                    icon: {
+//                        url: BASE_URI + '/js/images/map-crosshair.png',
+//                        anchor: { x: 35, y: 35 }
+//                    }
+//                });
+
+
+let map;
+async function initMap()    
+{
+    const { Map } = await google.maps.importLibrary("maps");
+    map = new Map(document.getElementById('map'), {
+    zoom:       17,
+    center:     {lat:   LATITUDE,   lng:    LONGITUDE },
+    mapId:      'myBloomington_map',
+    mapTypeId:  'satellite',
+    fullscreenControl: false,
+    streetViewControl: false,
+    mapTypeControl:    false
+    });
+}
