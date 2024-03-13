a1de26320b1f7082b24ab08aa81237f524edb02a
jhvhs-bloom <121638765+jhvhs-bloom@users.noreply.github.com>
2024-03-13 11:15:35 -0400

    map section with marker added

--- public/js/map-3.0.js
+++ public/js/map-3.0.js
@@ -1,32 +1,27 @@
 'use strict';
-// const coords = document.getElementById('coordinates');
-// const point  = { lat: parseFloat(coords.dataset.latitude), lng: parseFloat(coords.dataset.longitude) };
-// let   map    = new google.maps.Map(document.getElementById("map"), {
-//                    center:    point,
-//                    zoom:      17,
-//                    mapTypeId: 'satellite'
-//                });
-// const marker = new google.maps.Marker({
-//                    position: point,
-//                    map: map,
-//                    icon: {
-//                        url: BASE_URI + '/js/images/map-crosshair.png',
-//                        anchor: { x: 35, y: 35 }
-//                    }
-//                });
-
-
 let map;
 async function initMap()    
 {
     const { Map } = await google.maps.importLibrary("maps");
-    map = new Map(document.getElementById('map'), {
-    zoom:       17,
-    center:     {lat:   LATITUDE,   lng:    LONGITUDE },
-    mapId:      'myBloomington_map',
-    mapTypeId:  'satellite',
-    fullscreenControl: false,
-    streetViewControl: false,
-    mapTypeControl:    false
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
+
+    const map = new Map(document.getElementById('map'), {
+        zoom:       20,
+        center:     {lat:   LATITUDE,   lng:    LONGITUDE },
+        mapId:      'myBloomington_map',
+        mapTypeId:  'satellite',
+        fullscreenControl: false,
+        streetViewControl: false,
+        mapTypeControl:    false
     });
+
+    const pinScaled = new PinElement({
+        scale: 2.5,
+      });
+
+    const marker = new AdvancedMarkerElement({
+        map,
+        position: { lat:   LATITUDE,   lng:    LONGITUDE },
+        content: pinScaled.element,
+      });
 }
